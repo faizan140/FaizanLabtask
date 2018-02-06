b94a9a771e736164f1e7256938ba68de292166ad
AsadShahzad1 <35929059+AsadShahzad1@users.noreply.github.com>
2018-02-06 06:03:48 +0500

    .

---
+++ Lab 4/Task 1.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class factorial
+    {
+        public static int facto(int fact)
+        {
+            int result;
+            result = fact;
+            for (int i = 1; i < fact; i++)
+            {
+                result = result * i;
+            }
+                return result;
+            
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(factorial.facto(5));
+            Console.ReadLine();   
+        }
+    }
+}
